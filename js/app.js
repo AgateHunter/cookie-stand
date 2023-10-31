46ab5ff6e576a15b25d4b85c6c9841c86a40a744
NATALIE STONE <NATALIE.I.STONE@OUTLOOK.COM>
2023-10-30 20:46:33 -0700

    Updated js, css

---
+++ js/app.js
@@ -0,0 +1,19 @@
+
+
+const locationSection = document.getElementById("root");
+
+const seattleArea
+let seattleStats
+
+
+const tokyoArea
+let tokyoStats
+
+const dubaiArea
+let dubaiStats
+
+const parisArea
+let parisStats
+
+const limaArea
+let limaStats
